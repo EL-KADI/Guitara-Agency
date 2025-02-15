31f14623525c26b28e14126889e0e11964365bde
Start-Framework <sayedragheb92@gmail.com>
2025-02-16 01:12:09 +0200

    Guitara Agency

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,26 +3,19 @@ import logo from "../../Images/Guitara Agency Logo.png";
 import { useState, useEffect } from "react";
 
 export default function Navbar() {
-  const [isFirstGradient, setIsFirstGradient] = useState(true);
+  const [showFirstLink, setShowFirstLink] = useState(true);
 
   useEffect(() => {
-    const hours = new Date().getHours();
-    setIsFirstGradient(Math.floor(hours / 4) % 2 === 0);
-
     const interval = setInterval(() => {
-      setIsFirstGradient((prev) => !prev);
-    }, 4 * 60 * 60 * 1000);
+      setShowFirstLink((prev) => !prev);
+    }, 10 * 60 * 1000);
 
     return () => clearInterval(interval);
   }, []);
 
-  const gradientClass = isFirstGradient
-    ? "bg-gradient-to-r from-[#6A1B9A] to-[#E91E63]"
-    : "bg-gradient-to-br from-[#6A1B9A] to-[#1A2437]";
-
   return (
     <>
-      <nav className={gradientClass}>
+      <nav className="bg-gradient-to-r from-[#6A1B9A] to-[#E91E63]">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex items-center space-x-3 rtl:space-x-reverse lg:ms-32 md:ms-24 sm:ms-12 ms-6">
             <img
@@ -35,12 +28,23 @@ export default function Navbar() {
           <div className="block w-auto" id="navbar-solid-bg">
             <ul className="flex flex-col font-medium rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row custom-media mt-0 lg:me-24 me-12">
               <li>
-                <a
-                  href="#"
-                  className="block custom-font-family font-bold uppercase"
-                >
-                  Exclusive LiveStreaming Services
-                </a>
+                {showFirstLink ? (
+                  <a
+                    href="#"
+                    className="glowing-title block custom-font-family-tow font-bold uppercase"
+                  >
+                    G<span className="quick-blinking">u</span>it
+                    <span className="slow-blinking">a</span>r
+                    <span className="slow-blinking">a</span> Agency
+                  </a>
+                ) : (
+                  <a
+                    href="#"
+                    className="block custom-font-family font-bold uppercase"
+                  >
+                    Exclusive LiveStreaming Services
+                  </a>
+                )}
               </li>
             </ul>
           </div>
